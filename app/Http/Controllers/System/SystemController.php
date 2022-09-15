bf360802ee2515d3dba945589d861be34991a4e6
Ponsiano De Loor <ponsianodeloor@gmail.com>
2022-09-15 13:36:44 -0500

    redireccionar a la pagina system donde se encuentra la plantilla

--- app/Http/Controllers/System/SystemController.php
+++ app/Http/Controllers/System/SystemController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 
 class SystemController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth');
+    }
 
     public function index(){
         return view('system.index');
