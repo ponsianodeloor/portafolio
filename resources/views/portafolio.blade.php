a96d0c4618ba918acb1e64dcaeb210bd567d4bfd
Ponsiano De Loor <ponsianodeloor@gmail.com>
2022-10-04 10:50:26 -0500

    disenio previo al inicio de sesion con redes sociales

--- resources/views/portafolio.blade.php
+++ resources/views/portafolio.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Ponsiano De Loor - DevSecOps</title>
+    <title>{{$user->name}} - {{$user->about->about}}</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
@@ -71,9 +71,15 @@
 <section id="hero" class="d-flex flex-column justify-content-center align-items-center" style='background: url("{{asset($profile->url_photo_background)}}") top center;'>
     <div class="hero-container" data-aos="fade-in">
         <h1>{{$profile->user->name}}</h1>
-        <p>{{$profile->slogan}} <span class="typed" data-typed-items="{{$profile->slogan_dynamic}}"></span></p>
+        <p>{{$profile->slogan}} <span class="typed small" data-typed-items="{{$profile->slogan_dynamic}}"></span></p>
         <p>{{$profile->message}}</p>
+        <a href="" class="btn btn-primary bi bi-facebook"> Login With Facebook</a>
+        <a href="" class="btn btn-danger bi bi-google"> Login With Google</a>
+        <a href="" class="btn btn-light bi bi-github"> Login With GitHub</a>
+        <p>&nbsp;</p>
+        <p>This site was developed in Laravel 9</p>
     </div>
+
 </section><!-- End Hero -->
 
 <main id="main">
@@ -396,20 +402,6 @@
 </main><!-- End #main -->
 
 <!-- ======= Footer ======= -->
-<footer id="footer">
-    <div class="container">
-        <div class="copyright">
-            &copy; Copyright <strong><span>iPortfolio</span></strong>
-        </div>
-        <div class="credits">
-            <!-- All the links in the footer should remain intact. -->
-            <!-- You can delete the links only if you purchased the pro version. -->
-            <!-- Licensing information: https://bootstrapmade.com/license/ -->
-            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/ -->
-            Designed by <a href="https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/" target="_blank">BootstrapMade</a>
-        </div>
-    </div>
-</footer><!-- End  Footer -->
 
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
